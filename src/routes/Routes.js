2414c6e002658ee0920bd6041be5230f992baf3e
coca <cocacore7@gmail.com>
2022-08-14 18:30:20 -0600

    [ADD] información de desarrollador-201908335

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -7,7 +7,8 @@ router.get('/', (req, res) => {
 
     res.status(200).json({
         name : "Análisis y Diseño 1",
-        website1 : "José Abraham Solórzano Herrera - 201800937 - Práctica 1 - Grupo 8"
+        website1 : "José Abraham Solórzano Herrera - 201800937 - Práctica 1 - Grupo 8",
+        website2 : "Oscar René Rodríguez Vásquez - 201908335 - Práctica 1 - Grupo 8"
     });
 });
 
