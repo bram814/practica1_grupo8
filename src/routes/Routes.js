a2596a8a090deff9f4d3345a236efbe7606dbe5d
Bryan Paez <bryanpaez.125@gmail.com>
2022-08-14 18:44:57 -0600

    [ADD] Informacion de desarrollador-201700945

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,7 +9,9 @@ router.get('/', (req, res) => {
         name : "Análisis y Diseño 1",
         website1 : "José Abraham Solórzano Herrera - 201800937 - Práctica 1 - Grupo 8",
         website2 : "Oscar René Rodríguez Vásquez - 201908335 - Práctica 1 - Grupo 8",
-        website3 : "Uzzi Libni Aaron Pineda Solorzano - 201403541 - Práctica 1 - Grupo 8"
+        website3 : "Uzzi Libni Aaron Pineda Solorzano - 201403541 - Práctica 1 - Grupo 8",
+        website4 : "Bryan Gerardo Paez Morales - 201700945 - Práctica 1 - Grupo 8"
+
     });
 });
 
