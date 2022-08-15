25954e050690dddc0099d5e057f73176818afee8
Libni Solorzano <libnipineda@hotmail.com>
2022-08-14 18:41:16 -0600

    [ADD]Informacion de desarrollador-201403541

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,7 +8,8 @@ router.get('/', (req, res) => {
     res.status(200).json({
         name : "Análisis y Diseño 1",
         website1 : "José Abraham Solórzano Herrera - 201800937 - Práctica 1 - Grupo 8",
-        website2 : "Oscar René Rodríguez Vásquez - 201908335 - Práctica 1 - Grupo 8"
+        website2 : "Oscar René Rodríguez Vásquez - 201908335 - Práctica 1 - Grupo 8",
+        website3 : "Uzzi Libni Aaron Pineda Solorzano - 201403541 - Práctica 1 - Grupo 8"
     });
 });
 
